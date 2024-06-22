01dc2ccb29aec7a4e2fd2dc1731d21ef4c1ac980
Dharanidaran T <dharanidaran959@gmail.com>
2024-06-22 12:10:08 +0530

    Create file1.py

---
+++ quick pick data month wise/file1.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+url='https://raw.githubusercontent.com/dharanidaran-t/MarketBasketAnalysis/main/quick%20pick%20data%20month%20wise/april_df.csv'
+df1=pd.read_csv(url)
+print(df1)
