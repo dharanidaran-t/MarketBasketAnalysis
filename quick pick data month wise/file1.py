d5969f085c6fc1fe15e41b2e815518a600da2948
Dharanidaran T <dharanidaran959@gmail.com>
2024-06-22 12:26:14 +0530

    Update file1.py

--- quick pick data month wise/file1.py
+++ quick pick data month wise/file1.py
@@ -1,5 +1,14 @@
 import numpy as np
 import pandas as pd
-url='https://raw.githubusercontent.com/dharanidaran-t/MarketBasketAnalysis/main/quick%20pick%20data%20month%20wise/april_df.csv'
-df1=pd.read_csv(url)
-print(df1)
+
+#loading April dataset
+url1='https://raw.githubusercontent.com/dharanidaran-t/MarketBasketAnalysis/main/quick%20pick%20data%20month%20wise/april_df.csv'
+df1=pd.read_csv(url1)
+df1
+df1.info()
+#Checking for null values
+df1.isna().sum()
+
+#loading May dataset
+url2=''
+
