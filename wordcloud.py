530c674e5eec95ca931c3a817724d76ae79a5e9d
Dharanidaran T <dharanidaran959@gmail.com>
2024-06-28 11:59:00 +0530

    Update wordcloud.py
    cleaned leading characters

--- wordcloud.py
+++ wordcloud.py
@@ -6,17 +6,27 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 #Importing Dataset
-
 df = pd.read_csv("C:/Users/Win 10/Desktop/Market-Basket-Analysis/groceries_df.csv")
-#Renaming column
-df = df.rename(columns={'Item Name': 'title'})
-#Checking the Data
 
+# Function to strip leading characters- Batch names O,Q and Y and empty spaces
+import re
+
+def clean_item_name(item_name):
+    return re.sub(r'^[OQY]\s*', '', item_name)
+
+# Apply function to 'Item Name' column
+df['Item Name'] = df['Item Name'].apply(clean_item_name)
+df
+
+#Renaming column
+df = df.rename(columns={'Item Name': 'items'})
+
+#Checking the Data
 df.head()
 
 #Creating the text variable
 
-text2 = " ".join(title for title in df.title)
+text2 = " ".join(items for items in df.items)
 
 # Creating word_cloud with text as argument in .generate() method
 
